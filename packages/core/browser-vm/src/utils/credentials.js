15b5e7888a746b14dc74384c3f58a79bca8d1322
yr247308 <yr247308@alibaba-inc.com>
2024-03-29 18:38:03 +0800

    fix: fix getFetchCredentials

--- packages/core/browser-vm/src/utils/credentials.js
+++ packages/core/browser-vm/src/utils/credentials.js
@@ -1,3 +1,30 @@
+function isSameOriginUrl(str, origin) {
+  try {
+    // str 的格式可以被解析成 url
+    const url = new URL(str);
+    return url.origin === origin;
+  } catch (e) { /**/ }
+
+  try {
+    // str 是相对路径或绝对路径，无法被解析成 url
+    const url = new URL(str, origin);
+    return url.origin === origin;
+  } catch (e) {
+    return false;
+  }
+}
+
+// 某些情况下，请求静态资源需要带 cookie
 export const getFetchCredentials = (url) => {
-  return url.includes('console.aliyun.com') ? 'include' : 'omit';
+  // 兼容历史逻辑
+  if (url.includes('console.aliyun.com') || url.includes('console.alibabacloud.com')) {
+    return 'include';
+  }
+
+  // 同域请求
+  if (isSameOriginUrl(url, location && location.origin)) {
+    return 'include';
+  }
+
+  return 'omit';
 };
